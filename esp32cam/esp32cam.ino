44341789b66a8cf83eda6ab4f453e8e765b880bb
micheledinelli <dinellimichele00@gmail.com>
2025-09-22 17:56:41 +0200

    feat(esp32cam): update esp32cam.ino

--- esp32cam/esp32cam.ino
+++ esp32cam/esp32cam.ino
@@ -1,29 +1,17 @@
 
 #include "esp_camera.h"
-#include "FS.h"                // SD Card ESP32
-#include "SD_MMC.h"            // SD Card ESP32
-#include "soc/soc.h"           // Disable brownout problems
-#include "soc/rtc_cntl_reg.h"  // Disable brownout problems
+#include "FS.h"
+#include "SD_MMC.h"
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
 #include "driver/rtc_io.h"
-#include <WiFi.h>
-#include "time.h"
-
-
-// REPLACE WITH YOUR NETWORK CREDENTIALS
-const char* ssid = "6G-Radiation-Test-Node";
-const char* password = "casetta@BeaMich20-12";
 
 int lastState = LOW;
 unsigned long lastTriggerTime = 0;
-const unsigned long debounceDelay = 2000; 
-
-// REPLACE WITH YOUR TIMEZONE STRING: https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv
-String myTimezone = "CST6CDT,M3.2.0,M11.1.0";
+const unsigned long debounceDelay = 2000;
 
 #define TRIGGER_PIN 12
 
-// Pin definition for CAMERA_MODEL_AI_THINKER
-// Change pin definition if you're using another ESP32 camera module
 #define PWDN_GPIO_NUM 32
 #define RESET_GPIO_NUM -1
 #define XCLK_GPIO_NUM 0
@@ -41,12 +29,8 @@ String myTimezone = "CST6CDT,M3.2.0,M11.1.0";
 #define HREF_GPIO_NUM 23
 #define PCLK_GPIO_NUM 22
 
-
-// Stores the camera configuration parameters
 camera_config_t config;
 
-
-// Initializes the camera
 void configInitCamera() {
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
@@ -68,8 +52,8 @@ void configInitCamera() {
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
-  config.frame_size = FRAMESIZE_VGA;
-  config.pixel_format = PIXFORMAT_JPEG;  // for streaming
+  config.frame_size = FRAMESIZE_SVGA;
+  config.pixel_format = PIXFORMAT_JPEG;
   //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
   config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
   config.fb_location = CAMERA_FB_IN_PSRAM;
@@ -88,81 +72,30 @@ void configInitCamera() {
       config.fb_count = 2;
       config.grab_mode = CAMERA_GRAB_LATEST;
     } else {
-      // Limit the frame size when PSRAM is not available
       config.frame_size = FRAMESIZE_SVGA;
       config.fb_location = CAMERA_FB_IN_DRAM;
     }
   }
 
   sensor_t* s = esp_camera_sensor_get();
-  // initial sensors are flipped vertically and colors are a bit saturated
   if (s->id.PID == OV3660_PID) {
-    s->set_vflip(s, 1);        // flip it back
-    s->set_brightness(s, 1);   // up the brightness just a bit
-    s->set_saturation(s, -2);  // lower the saturation
+    s->set_vflip(s, 1);   
+    s->set_brightness(s, 1);  
+    s->set_saturation(s, -2);
   }
 
-  // drop down frame size for higher initial frame rate
   if (config.pixel_format == PIXFORMAT_JPEG) {
     s->set_framesize(s, FRAMESIZE_QVGA);
   }
-
-  Serial.println("ESP32CAM ready!");
 }
 
-
-// Connect to wifi
-void initWiFi() {
-  WiFi.begin(ssid, password);
-  Serial.println("Connecting Wifi");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-}
-
-
-// Function to set timezone
-void setTimezone(String timezone) {
-  Serial.printf("  Setting Timezone to %s\n", timezone.c_str());
-  setenv("TZ", timezone.c_str(), 1);  //  Now adjust the TZ.  Clock settings are adjusted to show the new local time
-  tzset();
-}
-
-
-// Connect to NTP server and adjust timezone
-void initTime(String timezone) {
-  struct tm timeinfo;
-  Serial.println("Setting up time");
-  configTime(0, 0, "pool.ntp.org");  // First connect to NTP server, with 0 TZ offset
-  if (!getLocalTime(&timeinfo)) {
-    Serial.println(" Failed to obtain time");
-    return;
-  }
-  Serial.println("Got the time from NTP");
-  // Now we can set the real timezone
-  setTimezone(timezone);
-}
-
-
-// Get the picture filename based on the current ime
 String getPictureFilename() {
-  struct tm timeinfo;
-  if (!getLocalTime(&timeinfo)) {
-    Serial.println("Failed to obtain time");
-    return "";
-  }
-  char timeString[20];
-  strftime(timeString, sizeof(timeString), "%Y-%m-%d_%H-%M-%S", &timeinfo);
-  Serial.println(timeString);
-  String filename = "/picture_" + String(timeString) + ".jpg";
+  unsigned long randNum = random(100000, 999999);
+  String filename = "/picture_" + String(randNum) + ".jpg";
   return filename;
 }
 
-
-// Initialize the micro SD card
 void initMicroSDCard() {
-  // Start Micro SD card
   Serial.println("Starting SD Card");
   if (!SD_MMC.begin("/sdcard", true)) {
     Serial.println("SD Card Mount Failed");
@@ -175,10 +108,7 @@ void initMicroSDCard() {
   }
 }
 
-
-// Take photo and save to microSD card
 void takeSavePhoto() {
-  // Take Picture with Camera
   camera_fb_t* fb = esp_camera_fb_get();
 
   //Uncomment the following lines if you're getting old pictures
@@ -192,12 +122,9 @@ void takeSavePhoto() {
     ESP.restart();
   }
 
-
-  // Path where new picture will be saved in SD Card
   String path = getPictureFilename();
   Serial.printf("Picture file name: %s\n", path.c_str());
 
-  // Save picture to microSD card
   fs::FS& fs = SD_MMC;
   File file = fs.open(path.c_str(), FILE_WRITE);
   if (!file) {
@@ -214,26 +141,23 @@ void takeSavePhoto() {
 void setup() {
   WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);  // disable brownout detector
 
-
   Serial.begin(115200);
   delay(2000);
 
-
-  // Initialize Wi-Fi
-  initWiFi();
-  // Initialize time with timezone
-  initTime(myTimezone);
-  // Initialize the camera
   Serial.print("Initializing the camera module...");
   configInitCamera();
   Serial.println("Ok!");
-  // Initialize MicroSD
+
   Serial.print("Initializing the MicroSD card module... ");
   initMicroSDCard();
+  Serial.println("Ok!");
 
   pinMode(TRIGGER_PIN, INPUT_PULLUP);
-}
 
+  randomSeed(esp_random());
+
+  Serial.println("ESP32CAM ready!");
+}
 
 void loop() {
   unsigned long now = millis();
@@ -242,7 +166,7 @@ void loop() {
   if (state == HIGH && lastState == LOW && (now - lastTriggerTime > debounceDelay)) {
     Serial.println("Signal received");
     takeSavePhoto();
-    
-    lastTriggerTime = now; 
+
+    lastTriggerTime = now;
   }
 }
