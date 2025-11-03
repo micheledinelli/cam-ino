aa81638f632db87f4dd1aa42b013494ab9e63995
micheledinelli <dinellimichele00@gmail.com>
2025-11-03 16:01:05 +0100

    feat(arduino-uno): update trigger distance

--- cam-ino/Config.h
+++ cam-ino/Config.h
@@ -5,7 +5,7 @@ const int HCSR04_TRIG_PIN = 11;  // Trigger pin for ultrasonic sensor HCSR04
 const int HCSR04_ECHO_PIN = 12;  // Echo pin for ultrasonic sensor HCSR04
 const int MAX_DISTANCE = 400;    // Maximum distance reachable from ultrasonic burst
 
-const int TRIGGER_DISTANCE_CM = 100; // Distance threshold to trigger the camera
+const int TRIGGER_DISTANCE_CM = 150; // Distance threshold to trigger the camera
 
 const int DHT_PIN = 2;  // DHT temperature sensor pin
 #define DHTTYPE DHT11   // DHT sensor type
